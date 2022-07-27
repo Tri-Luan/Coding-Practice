88b293d635ebb3a46f491ad69ae01b2b37e3b5ec
vitramkhoi2044 <vi.tk2044@sinhvien.hoasen.edu.vn>
2022-07-27 22:49:28 +0700

    update press enter login

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -126,6 +126,16 @@ const Login = () => {
       return information.FullName;
     }
   };
+
+  const pressEnterLogin = () => {
+    let input = document.getElementById("password");
+    input.addEventListener("keypress",function(event){
+      if(event.key === "Enter"){
+        document.getElementById("btn-login").click();
+      }
+    })
+  };
+
   return (
     <section className="h-screen">
       <div className="px-6 h-full text-gray-800">
@@ -164,11 +174,13 @@ const Login = () => {
                   className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                   id="password"
                   placeholder="Password"
+                  onKeyPress={() => pressEnterLogin()}
                 />
               </div>
               <div className="text-center lg:text-left">
                 <button
                   type="button"
+                  id="btn-login"
                   className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   onClick={() => handleLogin()}
                 >
